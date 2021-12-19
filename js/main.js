22deb1409ef69c0402c747693232d2877e4a707b
Unlimited23 <petyoruzhin@gmail.com>
2021-12-19 13:47:32 +0200

    add list rendering

--- js/main.js
+++ js/main.js
@@ -6,5 +6,16 @@ var app = new Vue({
         'link': 'https://www.vuemastery.com/',
         'inventory': 100,
         'onSale': true,
+        'details': ['80% cotton', '20% polyester', 'Gender-neutral'],
+        'variants': [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ]
     }
 });
